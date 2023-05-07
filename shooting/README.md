f49fd25ed19ae445a3f2c22827c9817bba80b7d3
Koichiro Mori <f2forest@gmail.com>
2023-05-07 21:34:07 +0900

    Add stage_manager

--- shooting/README.md
+++ shooting/README.md
@@ -2,6 +2,8 @@
 
 [Python / Pygame Tutorial: An introduction to sprites](https://www.youtube.com/watch?v=hDu8mcAlY4E)
 
+[Python / Pygame Tutorial: Creating multiple stages in a game](https://www.youtube.com/watch?v=j9yMFG3D7fg)
+
 ## Assets
 
 - [Shooting Gallery by Kenney](https://opengameart.org/content/shooting-gallery)
--- shooting/shooting_range.py
+++ shooting/shooting_range.py
@@ -27,8 +27,53 @@ class Target(pygame.sprite.Sprite):
         self.rect.center = (pos_x, pos_y)
 
 
+class GameState:
+    def __init__(self):
+        self.state = "intro"
+
+    def intro(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.state = "main_game"
+
+        # Drawing
+        screen.blit(background, (0, 0))
+        screen.blit(ready_text, (screen_width / 2 - 115, screen_height / 2 - 33))
+        player_group.draw(screen)
+        player_group.update()
+
+        pygame.display.flip()
+
+    def main_game(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                crosshair.shoot(target_group)
+
+        # Drawing
+        screen.blit(background, (0, 0))
+        target_group.draw(screen)
+        player_group.draw(screen)
+        player_group.update()
+
+        pygame.display.flip()
+
+    def state_manager(self):
+        if self.state == "intro":
+            self.intro()
+        if self.state == "main_game":
+            self.main_game()
+
+
 pygame.init()
 clock = pygame.time.Clock()
+game_state = GameState()
 
 # Game Screen
 screen_width = 1024
@@ -36,12 +81,13 @@ screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 background = pygame.image.load("BG.png")
+ready_text = pygame.image.load("ready_text.png")
 pygame.mouse.set_visible(False)
 
 # Crosshair
 crosshair = Crosshair("crosshair.png")
-crosshair_group = pygame.sprite.Group()
-crosshair_group.add(crosshair)
+player_group = pygame.sprite.Group()
+player_group.add(crosshair)
 
 # Target
 target_group = pygame.sprite.Group()
@@ -54,19 +100,5 @@ for _ in range(20):
     target_group.add(target)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            crosshair.shoot(target_group)
-
-    screen.blit(background, (0, 0))
-
-    target_group.draw(screen)
-    crosshair_group.draw(screen)
-    crosshair_group.update()
-
-    pygame.display.flip()
+    game_state.state_manager()
     clock.tick(60)
