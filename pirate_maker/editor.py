19dbeacdc99d87aa97e6204ae03c65b317f24f2a
Koichiro Mori <f2forest@gmail.com>
2023-05-08 21:25:44 +0900

    Draw support lines

--- pirate_maker/editor.py
+++ pirate_maker/editor.py
@@ -4,6 +4,7 @@ import pygame
 from pygame.math import Vector2 as vector
 from pygame.mouse import get_pos as mouse_pos
 from pygame.mouse import get_pressed as mouse_buttons
+from settings import LINE_COLOR, TILE_SIZE, WINDOW_HEIGHT, WINDOW_WIDTH
 
 
 class Editor:
@@ -16,6 +17,13 @@ class Editor:
         self.origin = vector()
         self.pan_active = False
         self.pan_offset = vector()
+
+        # support lines
+        # マス目を描くSurfaceを別に作る
+        # alphaで透明色を制御するため
+        self.support_line_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.support_line_surf.set_colorkey("green")
+        self.support_line_surf.set_alpha(30)
 
     # input
     def event_loop(self):
@@ -49,9 +57,44 @@ class Editor:
         if self.pan_active:
             self.origin = vector(mouse_pos()) - self.pan_offset
 
+    def draw_tile_lines(self):
+        cols = WINDOW_WIDTH // TILE_SIZE
+        rows = WINDOW_HEIGHT // TILE_SIZE
+
+        # originを移動したときに左側と右側にも描画されるように調整
+        origin_offset = vector(
+            x=self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
+            y=self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE,
+        )
+
+        self.support_line_surf.fill("green")
+
+        # +1はoriginを動かしたときに最右側でも表示されるように
+        for col in range(cols + 1):
+            x = origin_offset.x + col * TILE_SIZE
+            pygame.draw.line(
+                self.support_line_surf,
+                color=LINE_COLOR,
+                start_pos=(x, 0),
+                end_pos=(x, WINDOW_HEIGHT),
+            )
+
+        for row in range(rows + 1):
+            y = origin_offset.y + row * TILE_SIZE
+            pygame.draw.line(
+                self.support_line_surf,
+                color=LINE_COLOR,
+                start_pos=(0, y),
+                end_pos=(WINDOW_WIDTH, y),
+            )
+
+        self.display_surface.blit(self.support_line_surf, (0, 0))
+
     def run(self, dt):
-        self.display_surface.fill("white")
         self.event_loop()
+
+        self.display_surface.fill("white")
+        self.draw_tile_lines()
         pygame.draw.circle(
             self.display_surface, color="red", center=self.origin, radius=10
         )
