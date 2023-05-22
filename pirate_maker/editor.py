ee60b903068242ba963feda9b5211718f33c5783
Koichiro Mori <f2forest@gmail.com>
2023-05-22 21:19:53 +0900

    Exporting the editor data

--- pirate_maker/editor.py
+++ pirate_maker/editor.py
@@ -89,9 +89,12 @@ class Editor:
         )
 
     # support
-    def get_current_cell(self):
-        """マウスがある位置のセル（row, col）を取得する"""
-        distance_to_origin = vector(mouse_pos()) - self.origin
+    def get_current_cell(self, obj=None):
+        """マウスまたはオブジェクトがある位置のセル（row, col）を取得する"""
+        if not obj:
+            distance_to_origin = vector(mouse_pos()) - self.origin
+        else:
+            distance_to_origin = vector(obj.distance_to_origin) - self.origin
 
         if distance_to_origin.x > 0:
             col = int(distance_to_origin.x / TILE_SIZE)
@@ -185,12 +188,86 @@ class Editor:
             if sprite.rect.collidepoint(mouse_pos()):
                 return sprite
 
+    def create_grid(self):
+        # add objects to the tiles
+
+        for tile in self.canvas_data.values():
+            tile.objects = []
+
+        for obj in self.canvas_objects:
+            current_cell = self.get_current_cell(obj)
+            # オブジェクトがあるセルの座標からオブジェクトの座標へのベクトル
+            offset = vector(obj.distance_to_origin) - (vector(current_cell) * TILE_SIZE)
+
+            # CanvasTileとしてオブジェクトを追加
+            if current_cell in self.canvas_data:
+                # タイルがすでにある場合
+                self.canvas_data[current_cell].add_id(obj.tile_id, offset)
+            else:
+                self.canvas_data[current_cell] = CanvasTile(obj.tile_id, offset)
+
+        # create an empty grid
+        layers = {
+            "water": {},
+            "bg palms": {},
+            "terrain": {},
+            "enemies": {},
+            "coins": {},
+            "fg objects": {},
+        }
+
+        # grid offset
+        # 一番左にあるタイルやオブジェクトのx座標を取得
+        left = sorted(self.canvas_data.keys(), key=lambda tile: tile[0])[0][0]
+        # 一番上にあるタイルやオブジェクトのy座標を取得
+        top = sorted(self.canvas_data.keys(), key=lambda tile: tile[1])[0][1]
+
+        # fill the grid
+        for tile_pos, tile in self.canvas_data.items():
+            # 一番左上にあるタイルを (0, 0) になるように調整する
+            row_adjusted = tile_pos[1] - top
+            col_adjusted = tile_pos[0] - left
+            x = col_adjusted * TILE_SIZE
+            y = row_adjusted * TILE_SIZE
+
+            if tile.has_water:
+                layers["water"][(x, y)] = tile.get_water()
+
+            if tile.has_terrain:
+                layers["terrain"][(x, y)] = (
+                    tile.get_terrain() if tile.get_terrain() in self.land_tiles else "X"
+                )
+
+            if tile.coin:
+                layers["coins"][(x + TILE_SIZE // 2, y + TILE_SIZE // 2)] = tile.coin
+
+            if tile.enemy:
+                layers["enemies"][(x, y)] = tile.enemy
+
+            if tile.objects:
+                for obj, offset in tile.objects:
+                    if obj in [
+                        key
+                        for key, value in EDITOR_DATA.items()
+                        if value["style"] == "palm_bg"
+                    ]:
+                        layers["bg palms"][(int(x + offset.x), int(y + offset.y))] = obj
+                    else:
+                        layers["fg objects"][
+                            (int(x + offset.x), int(y + offset.y))
+                        ] = obj
+
+        return layers
+
     # input
     def event_loop(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                print(self.create_grid())
 
             # マウスによる原点の移動
             self.pan_input(event)
@@ -581,7 +658,7 @@ class Editor:
 
 
 class CanvasTile:
-    def __init__(self, tile_id):
+    def __init__(self, tile_id, offset=vector()):
         # terrain
         self.has_terrain = False
         # 隣にタイルがあったら丸まった地形に表示するため
@@ -601,10 +678,10 @@ class CanvasTile:
         # objects
         self.objects = []
 
-        self.add_id(tile_id)
+        self.add_id(tile_id, offset=offset)
         self.is_empty = False
 
-    def add_id(self, tile_id):
+    def add_id(self, tile_id, offset=vector()):
         options = {key: value["style"] for key, value in EDITOR_DATA.items()}
         match options[tile_id]:
             case "terrain":
@@ -615,6 +692,9 @@ class CanvasTile:
                 self.coin = tile_id
             case "enemy":
                 self.enemy = tile_id
+            case _:  # objects
+                if (tile_id, offset) not in self.objects:
+                    self.objects.append((tile_id, offset))
 
     def remove_id(self, tile_id):
         options = {key: value["style"] for key, value in EDITOR_DATA.items()}
@@ -638,6 +718,12 @@ class CanvasTile:
             and not self.enemy
         ):
             self.is_empty = True
+
+    def get_water(self):
+        return "bottom" if self.water_on_top else "top"
+
+    def get_terrain(self):
+        return "".join(self.terrain_neighbors)
 
 
 class CanvasObject(pygame.sprite.Sprite):
